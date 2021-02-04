fa6c9521291e963be0f51573d9365d3c9ac6740d
ruchik-p-simformsolutions <ruchik.p@simformsolutions.com>
2021-02-04 16:58:12 +0530

    completed module basic started with file system

--- Week5NodeJS/app.js
+++ Week5NodeJS/app.js
@@ -1,5 +1,23 @@
 //require
+//loading OS file system in node application
+//require('fs') is node module
+const fs=require("fs");
 const val=require("./moduleBasic");
+const validator=require("validator");
+const chalk=require("chalk");
+
+//writeFileSync used for writing into file asynchronous
+fs.writeFileSync("sample.txt","hello world");
+//this will override
+//fs.writeFileSync("sample.txt","hello all");
+//appendFileSync will append content to existing file 
+fs.appendFileSync("sample.txt","this is sample file");
+
 console.log(val.PI);
-console.log(val.add);
+console.log(chalk.blue(val.add(3,5)));
 console.log(new val.Employee("james richardson"));
+
+console.log(validator.isEmail("abc@gmail.com"));
+console.log(validator.isURL("wwwgooglecom"));
+const warning = chalk.keyword('orange');
+console.log(warning('warning!'));
