fe94254d4af0947306e33188931367cf3fd4a471
ruchik-p-simformsolutions <ruchik.p@simformsolutions.com>
2021-02-16 20:15:23 +0530

    weather app files(forecast,geocode) added

--- Week5NodeJS/asynchronous.js
+++ Week5NodeJS/asynchronous.js
@@ -1,18 +1,17 @@
 const request = require("request");
-const url =
-  "http://api.weatherstack.com/current?access_key=c0e873690f90f9b02e825614197efe43&query=18.9986,72.8174?&units=f";
-request({ url: url, json: true }, (error, response) => {
-  //
-  if (error) console.log("not connected to internet");
-  else if (response.body.error) console.log("not able to locate");
-  else console.log(response.body.current["temperature"]);
-});
+const geocode = require("./geocode");
+const forecast = require("./forecast");
 
-const url2 =
-  "https://api.mapbox.com/geocoding/v5/mapbox.places/San%20Francisco.json?access_token=pk.eyJ1IjoiYWJjNDA3NDU1IiwiYSI6ImNrbDZjczYweDI3bjEydWxiMGEzcHpuZzUifQ.NsrFrr2E5jFfE_4rLUv_ZQ&limit=1";
-request({ url: url2, json: true }, (error, response) => {
-  if (error) console.log("not connected to internet");
-  else if (response.body.features.length === 0)
-    console.log("not able to find latitude and longitude");
-  else console.log(response.body.features[0].center);
-});
+const location = process.argv[2];
+
+if (!location) console.log("enter location...");
+else {
+  geocode.geolocation(location, (error, {latitude,longitude}={}) => {
+    if (error) return console.log(error);
+    forecast.weather(latitude, longitude, (error, {temperature,location}={}) => {
+      if (error) return console.log(error);
+      console.log(location);
+      console.log(temperature);
+    });
+  });
+}
