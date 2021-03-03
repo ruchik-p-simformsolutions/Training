da69c95307ab7841ad1190311c6a5246fc6066d7
Ruchik.Pravasi <ruchik.p@simformsolutions.com>
2021-03-03 11:13:33 +0530

    sorting, pagination and filtering

--- Week5NodeJS/task-manager/src/routers/task.js
+++ Week5NodeJS/task-manager/src/routers/task.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const auth=require('../middleware/auth');
+const auth = require('../middleware/auth');
 const Task = require("../models/task");
-const User=require("../models/user");
+const User = require("../models/user");
 
-router.post("/tasks", auth,async (req, res) => {
-  const task=new Task({
+router.post("/tasks", auth, async (req, res) => {
+  const task = new Task({
     ...req.body,
-    owner:req.user._id,
+    owner: req.user._id,
   })
   try {
     await task.save();
@@ -17,9 +17,26 @@ router.post("/tasks", auth,async (req, res) => {
   }
 });
 
-router.get("/tasks",auth, async (req, res) => {
+router.get("/tasks", auth, async (req, res) => {
+  const match = {};
+  const sort = {};
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true';
+  }
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':');
+    sort[parts[0]] = parts[1] === 'aesc' ? 1 : -1;
+  }
   try {
-    await req.user.populate('tasks').execPopulate();
+    await req.user.populate({
+      path: 'tasks',
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort
+      }
+    }).execPopulate();
     res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send(e);
@@ -27,10 +44,10 @@ router.get("/tasks",auth, async (req, res) => {
 });
 
 router.get("/tasks/:id", auth, async (req, res) => {
-  const _id=req.params.id;
+  const _id = req.params.id;
   try {
-    const task = await Task.findOne({_id,owner:req.user._id});
-    if(!task){
+    const task = await Task.findOne({ _id, owner: req.user._id });
+    if (!task) {
       res.status(401).send();
     }
     res.status(201).send(task);
@@ -39,22 +56,22 @@ router.get("/tasks/:id", auth, async (req, res) => {
   }
 });
 
-router.patch("/tasks/:id", auth,async (req, res) => {
+router.patch("/tasks/:id", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const listOfTask = ["description", "completed"];
   const isValid = updates.every((update) => listOfTask.includes(update));
   if (!isValid) return res.status(400).send({ error: "invalid updates" });
-  const _id=req.params.id;
+  const _id = req.params.id;
   try {
-    const task=await Task.findOne({
+    const task = await Task.findOne({
       _id,
-      owner:req.user._id
+      owner: req.user._id
     });
 
     if (!task) return res.status(404).send();
 
-    updates.forEach((update)=>{
-      task[update]=req.body[update];
+    updates.forEach((update) => {
+      task[update] = req.body[update];
     })
     await task.save();
     res.status(201).send(task);
@@ -63,11 +80,11 @@ router.patch("/tasks/:id", auth,async (req, res) => {
   }
 });
 
-router.delete("/tasks/:id", auth,async (req, res) => {
-  const _id=req.params.id;
+router.delete("/tasks/:id", auth, async (req, res) => {
+  const _id = req.params.id;
   try {
     const task = await Task.findOneAndDelete({
-      _id,owner:req.user._id
+      _id, owner: req.user._id
     });
     if (!task) return res.status(404).send();
     res.status(201).send(task);
