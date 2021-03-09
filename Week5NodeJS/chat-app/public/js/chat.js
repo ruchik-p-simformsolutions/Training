9d7a8cf15bb8cd355d654805c81cb38cfcf9479f
Ruchik.Pravasi <ruchik.p@simformsolutions.com>
2021-03-09 18:24:35 +0530

    socket.io

--- Week5NodeJS/chat-app/public/js/chat.js
+++ Week5NodeJS/chat-app/public/js/chat.js
@@ -1,16 +1,34 @@
 const socket=io();  
-// socket.on('newUser',(str)=>{
-//     console.log(str);
-// })
+socket.on('newUser',(str)=>{
+    console.log(str);
+})
 
+const $messages=document.querySelector('#messages');
+const $messageTemplate=document.querySelector('#message-template').innerHTML;
+const $locationTemplate=document.querySelector('#location-template').innerHTML;
 
 socket.on('msgforAll',(message)=>{
     console.log(message);
+    const html=Mustache.render($messageTemplate,{
+        message:message.text,
+        createdAt:moment(message.createdAt).format('HH:mm:ss'),
+    });
+    $messages.insertAdjacentHTML("beforeend",html);
+})
+
+socket.on('locationMsg',(message)=>{
+    console.log(message.url);
+    const html=Mustache.render($locationTemplate,{
+        url:message.url,
+        createdAt:moment(message.createdAt).format('HH:mm:ss'),
+    })
+    $messages.insertAdjacentHTML('beforeend',html);
 })
 
 document.getElementById("messageform").addEventListener("submit", (e)=>{;
 
     e.preventDefault()
+    document.getElementById('msgSubButton').disabled=true;
     // var ms=document.getElementById('inp').value;
     var ms=e.target.elements.msg.value;    
     socket.emit("sendAll",ms,(error)=>{
@@ -18,6 +36,9 @@ document.getElementById("messageform").addEventListener("submit", (e)=>{;
             return console.log(error);
         }
         console.log("msg delivered");
+        document.getElementById('inp').value='';
+        document.getElementById('inp').focus();
+        document.getElementById('msgSubButton').disabled=false;
     });
 
 })
@@ -27,6 +48,7 @@ document.getElementById('sendLocation').addEventListener('click',()=>{
     if(!navigator.geolocation){
         return alert('no geolocation ');
     }
+    document.getElementById('sendLocation').disabled=true;
     navigator.geolocation.getCurrentPosition((position)=>{
         // console.log(position.coords.latitude);
         socket.emit('sendLocation',{
@@ -34,6 +56,7 @@ document.getElementById('sendLocation').addEventListener('click',()=>{
             longitude:position.coords.longitude,
         },()=>{
             console.log("location shared");
+            document.getElementById('sendLocation').disabled=false;
         })
     })
 })
